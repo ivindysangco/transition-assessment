29e92e134c2aeb6695ac89a86a5bd5824acdff78
ivindysangco <44565169+ivindysangco@users.noreply.github.com>
2021-08-13 08:13:30 -0400

    new changes

--- test_area/main.js
+++ test_area/main.js
@@ -21,8 +21,8 @@ function radioButtonLoop(){
 			selfAware[a] += '<input type="radio" value="' + i + '" name="selfAwareQ' + a + '" class="radio-btn">' + i;
 			//document.getElementById("radioLoop").innerHTML = selfAware[5];
 		}
-		//prints the code from js to html under selfAwQ
-		document.getElementById("selfAwQ" + a).innerHTML = selfAware[a];
+		//prints the code from js to html under container1_self_aware_radio_btn
+		document.getElementById("container1_self_aware_radio_btn" + a).innerHTML = selfAware[a];
 	}
 }
 
@@ -35,10 +35,10 @@ function check() {
   document.getElementById("fName").textContent = "First Name: " + fName;
   document.getElementById("lName").textContent = "Last Name: " + lName;
 
-  var selfAwareTitle = document.getElementById("section_title").textContent;
-  document.getElementById("self_aware_title").innerHTML = selfAwareTitle;
+  var selfAwareTitle = document.getElementById("container1_self_aware_title").textContent;
+  document.getElementById("container2_self_aware_title").innerHTML = selfAwareTitle;
 
-//  var selfWareQT1 = document.getElementById("selfAwareQText1").textContent;
+//  var selfWareQT1 = document.getElementById("container1_self_aware_question1").textContent;
 //  document.getElementById("print-self-aware-q-1").innerHTML = selfWareQT1;
 
 	var selected;
@@ -53,38 +53,38 @@ function check() {
     //gets the value of the radio
 		selected =  Array.from(document.getElementsByName("selfAwareQ" + i)).find(radio => radio.checked);
     //gets the text of question from container 1
-    selfWareQT = document.getElementById("selfAwareQText" + i).textContent;
+    selfWareQT = document.getElementById("container1_self_aware_question" + i).textContent;
     //prints the questions to container 2
-    document.getElementById("print_self_aware_q_" + i).innerHTML = selfWareQT + parseInt(selected.value);
+    document.getElementById("container2_self_aware_title_question_" + i).innerHTML = selfWareQT + parseInt(selected.value);
     //gets the value of each radio button and add them all up
 		selfAwareTotal += parseInt(selected.value);
     //prints the total TRQ
-		document.getElementById("result").textContent = "SelfAwareTotal is " +  selfAwareTotal;
+		document.getElementById("container1_self_aware_result").textContent = "SelfAwareTotal is " +  selfAwareTotal;
 	}
 	selfAwareTRQ = parseFloat(selfAwareTotal / 8 / 2);
 	//result.textContent= 'Self-awareness Transition Readiness Quotient (TRQ) is ' + (selfAwareTotal / 8 / 2);
-	document.getElementById("result").textContent = "Your self-awareness TRQ is " +  selfAwareTRQ;
-  document.getElementById("print_result").textContent = "Your self-awareness TRQ is " +  selfAwareTRQ;
+	document.getElementById("container1_self_aware_result").textContent = "Your self-awareness TRQ is " +  selfAwareTRQ;
+  document.getElementById("container2_print_self_aware_results").textContent = "Your self-awareness TRQ is " +  selfAwareTRQ;
 	if(selfAwareTRQ <= 2.99)
 	{
-		document.getElementById("result").style.color = "red";
-    document.getElementById("print_result").style.color = "red";
+		document.getElementById("container1_self_aware_result").style.color = "red";
+    document.getElementById("container2_print_self_aware_results").style.color = "red";
 	}
 	else if (selfAwareTRQ > 2.99 && selfAwareTRQ <= 3.99)
 	{
-		document.getElementById("result").style.color = "orange";
-    document.getElementById("print_result").style.color = "orange";
+		document.getElementById("container1_self_aware_result").style.color = "orange";
+    document.getElementById("container2_print_self_aware_results").style.color = "orange";
 	}
 	else if (selfAwareTRQ >3.99 && selfAwareTRQ <= 5)
 	{
-		document.getElementById("result").style.color = "green";
-    document.getElementById("print_result").style.color = "green";
+		document.getElementById("container1_self_aware_result").style.color = "green";
+    document.getElementById("container2_print_self_aware_results").style.color = "green";
 	}
 }
 
 function HTMLtoPDF(){
   var doc = new jsPDF();
-  var HTMLelement = $(".print-area").html();
+  var HTMLelement = $(".pdf_area").html();
 
   doc.fromHTML(HTMLelement, 15, 15,{
     'width':190
@@ -93,19 +93,19 @@ function HTMLtoPDF(){
   doc.save("transition_assessment.pdf");
 }
 /*
-const steps = Array.from(document.querySelectorAll("form .step"));
+const containerDisplays = Array.from(document.querySelectorAll("form .containerDisplay"));
 const nextBtn = document.querySelectorAll("form .next-btn");
 const prevBtn = document.querySelectorAll("form .previous-btn");
 const form = document.querySelector("form");
 
 nextBtn.forEach((button) => {
   button.addEventListener("click", () => {
-    changeStep("next");
+    changecontainerDisplay("next");
   });
 });
 prevBtn.forEach((button) => {
   button.addEventListener("click", () => {
-    changeStep("prev");
+    changecontainerDisplay("prev");
   });
 });
 
@@ -120,21 +120,21 @@ form.addEventListener("submit", (e) => {
   form.reset();
   let index = 0;
   const active = document.querySelector(".active");
-  index = steps.indexOf(active);
-  steps[index].classList.remove("active");
-  steps[0].classList.remove("active");
+  index = containerDisplays.indexOf(active);
+  containerDisplays[index].classList.remove("active");
+  containerDisplays[0].classList.remove("active");
 });
 
-function changeStep(btn) {
+function changecontainerDisplay(btn) {
   let index = 0;
   const active = document.querySelector(".active");
-  index = steps.indexOf(active);
-  steps[index].classList.remove("active");
+  index = containerDisplays.indexOf(active);
+  containerDisplays[index].classList.remove("active");
   if (btn === "next") {
     index++;
   } else if (btn === "prev") {
     index--;
   }
-  steps[index].classList.add("active");
+  containerDisplays[index].classList.add("active");
 }
 */
